294e06fc4869d6b2beda98b843abb9057fddb7b5
ytyaru <yttry0@gmail.com>
2024-12-11 19:51:07 +0900

    add 2

---
+++ docs/2/js/main.js
@@ -0,0 +1,51 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOMContentLoaded!!');
+    const {h1, p, a} = van.tags
+    const author = 'ytyaru'
+    van.add(document.querySelector('main'), 
+        h1(a({href:`https://github.com/${author}/Html.VanJS.Ruby.Sugar.20241211160504/`}, 'Ruby.Sugar')),
+        p('HTMLのRubyを簡易生成する。'),
+//        p('Easily generate HTML Ruby.'),
+    )
+    van.add(document.querySelector('footer'),  new Footer('ytyaru', '../').make())
+
+    console.log(ShortRubyParser.parse('漢字《かんじ》'))
+//    console.log(UnderShortRubyParser.parse('↓漢字《かんじ》'))
+    console.log(LongRubyParser.parse('↓漢字《かんじ》'))
+    console.log(ShortRubyParser.parse('あ漢字《かんじ》い'))
+//    console.log(UnderShortRubyParser.parse('あ↓漢字《かんじ》い'))
+    console.log(LongRubyParser.parse('あ↓漢字《かんじ》い'))
+    console.log(ShortRubyParser.parse('漢字《かんじ｜カンジ》'))
+    console.log(ShortRubyParser.parse('あ漢字《かんじ｜カンジ》い'))
+    console.log(ShortRubyParser.parse('短い漢字《かんじ》が複数《ふくすう》あるよ'))
+
+    console.log(LongRubyParser.parse('連続｜漢字《かんじ》が｜複数《ふくすう》あるよ'))
+
+    console.log(LongRubyParser.parse('｜論救の御手《ろんきゅう｜みて》'))
+    console.log(LongRubyParser.parse('｜論救の御手《ろんきゅう｜みて｜ロジカル・サルベータ》'))
+    console.log(LongRubyParser.parse('↓論救の御手《ろんきゅう｜みて｜ロジカル・サルベータ》'))
+
+    console.log(LongRubyParser.parse('｜あいうえお《AIUEO》'))
+    console.log(LongRubyParser.parse('｜漢字《かんじ》'))
+    console.log(LongRubyParser.parse('↓漢字《かんじ》'))
+    console.log(LongRubyParser.parse('｜ああ複数の漢字にルビ一つだけ《ふくすう》'))
+    // 想定外↓↓↓↓↓
+    console.log(LongRubyParser.parse('｜漢字《》'))
+    console.log(LongRubyParser.parse('｜漢字《》'))
+    console.log(LongRubyParser.parse('｜漢字《｜》'))
+    console.log(LongRubyParser.parse('｜漢字《か｜カ》'))
+    console.log(LongRubyParser.parse('｜漢字《か｜》'))
+    console.log(LongRubyParser.parse('｜漢字《｜カ》'))
+    console.log(LongRubyParser.parse('｜漢字《か｜カ｜キ》'))
+    // 想定外↑↑↑↑↑
+    console.log(ShortRubyParser.parse('山《やま》田《だ》'))
+    console.log(ShortRubyParser.parse('太《た》郎《ろう》'))
+    console.log(ShortRubyParser.parse('山田《やまだ》太郎《たろう》'))
+    console.log(ShortRubyParser.parse('山田太郎《やまだたろう》'))
+//    console.log(ShortRubyParser.parse('山田太郎《やま｜だ｜た｜ろう｜やまだたろう》')) // 不可能！！！
+    console.log(ShortRubyParser.parse('山田太郎《やまだたろう｜Yamada Tarou》'))
+});
+window.addEventListener('beforeunload', (event) => {
+    console.log('beforeunload!!');
+});
+
