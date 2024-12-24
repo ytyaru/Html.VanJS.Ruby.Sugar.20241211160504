4fc9193462011cedbbc8c4a48ae896c2b14bfdb4
ytyaru <yttry0@gmail.com>
2024-12-24 11:54:57 +0900

    add 30 完成

---
+++ docs/30/test/main.js
@@ -0,0 +1,145 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOMContentLoaded!!');
+//    const {h1, p, a} = van.tags
+    const author = 'ytyaru'
+    van.add(document.querySelector('main'), 
+        van.tags.h1(van.tags.a({href:`https://github.com/${author}/Html.VanJS.Ruby.Sugar.20241211160504/`}, 'Ruby.Sugar')),
+        van.tags.p('HTMLのRubyを簡易生成する。'),
+//        p('Easily generate HTML Ruby.'),
+    )
+    van.add(document.querySelector('footer'),  new Footer('ytyaru', '../').make())
+
+    // Ruby(Short, Long, Escape), Em(Escape)
+    const a = new Assertion()
+    const bb = new BlackBox(a)
+    a.t(()=>RubyParser.parse('漢字《かんじ》')==='<ruby>漢字<rp>（</rp><rt>かんじ</rt><rp>）</rp></ruby>')
+    const table = UnitTest.test(a)
+    a.fin()
+    document.querySelector(`main`).append(table)
+    document.querySelector(`textarea`).focus()
+
+    console.log(RubyParser.parse('漢字《かんじ》'))
+    console.log(RubyParser.parse('漢字《かん,じ》'))
+    console.log(RubyParser.parse('漢字《かんじ｜カンジ》'))
+    console.log(RubyParser.parse('漢字《かん,じ,超過》')) // 超過分ルビは無視してHTML化する
+    console.log(RubyParser.parse('漢字《かん,じ｜KAN,JI》'))
+    console.log(RubyParser.parse('↓漢字《かんじ》'))
+    console.log(RubyParser.parse('あ漢字《かんじ》い'))
+    console.log(RubyParser.parse('あ↓漢字《かんじ》い'))
+    console.log(RubyParser.parse('あ漢字《かんじ｜カンジ》い'))
+    console.log(RubyParser.parse('短い漢字《かんじ》が複数《ふくすう》あるよ'))
+    console.log(RubyParser.parse('連続｜漢字《かんじ》が｜複数《ふくすう》あるよ'))
+
+    console.log(RubyParser.parse('｜論救の御手《ろんきゅう｜みて》'))
+    console.log(RubyParser.parse('｜論救の御手《ろんきゅう｜みて｜ロジカル・サルベータ》'))
+    console.log(RubyParser.parse('↓論救の御手《ろんきゅう｜みて｜ロジカル・サルベータ》'))
+    console.log(RubyParser.parse('｜論救の御手《ろんきゅう｜みて｜ロジカル・サルベータ｜無視される》'))
+    console.log(RubyParser.parse('｜論救の御手《ろん,きゅう｜み,て｜ロジカル・サルベータ》'))
+    console.log(RubyParser.parse('↓論救の御手《ろん,きゅう｜み,て｜ロジカル・サルベータ》'))
+    console.log(RubyParser.parse('｜論救の御手《ろん,きゅう｜みて｜ロジカル・サルベータ》'))
+
+    console.log(RubyParser.parse('｜あいうえお《AIUEO》'))
+    console.log(RubyParser.parse('｜漢字《かんじ》'))
+    console.log(RubyParser.parse('↓漢字《かんじ》'))
+    console.log(RubyParser.parse('｜ああ複数の漢字群にルビ一つだけ《ふくすう》'))
+    console.log(RubyParser.parse('↓ああ複数の漢字群にルビ一つだけ《ふくすう》'))
+    console.log(RubyParser.parse('｜ああ複数の漢字群にルビ二つだけ《ふくすう｜かんじぐん》'))
+    console.log(RubyParser.parse('｜ああ複数の漢字群にルビ三つだけ《ふくすう｜かんじぐん｜みっ》'))
+    console.log(RubyParser.parse('｜ああ複数の漢字群にルビ四つだけ《ふくすう｜かんじぐん｜よっ｜全体ルビ》'))
+    console.log(RubyParser.parse('｜ああ複数の漢字群にルビ五つだけ《ふくすう｜かんじぐん｜いつ｜全体ルビ｜無視される》'))
+
+    // 漢字群を無視して強制的に上下単一全体ルビ
+    console.log(RubyParser.parse('｜あ複数い漢字群う《うえ↓した》'))
+    console.log(RubyParser.parse('↓あ複数い漢字群う《した↑うえ》'))
+    console.log(RubyParser.parse('｜あ複数い漢字群う《うえ↑した》'))
+    console.log(RubyParser.parse('↓あ複数い漢字群う《した↓うえ》'))
+
+    // 想定外↓↓↓↓↓
+    console.log(RubyParser.parse('｜《》'))
+    console.log(RubyParser.parse('｜｜《》'))
+    console.log(RubyParser.parse('｜漢字《》'))
+    console.log(RubyParser.parse('｜漢字《,》'))
+    console.log(RubyParser.parse('｜漢字《,じ》'))
+    console.log(RubyParser.parse('｜漢字《かん,》'))
+    console.log(RubyParser.parse('｜漢字《,,》'))
+    console.log(RubyParser.parse('｜漢字《｜》'))
+    console.log(RubyParser.parse('｜漢字《か｜》'))
+    console.log(RubyParser.parse('｜漢字《｜カ》'))
+    console.log(RubyParser.parse('｜漢字《｜｜》'))
+    console.log(RubyParser.parse('｜漢字《｜｜KA》'))
+    console.log(RubyParser.parse('｜漢字《かんじ｜カンジ｜KANJI》')) // 二つ目以降の｜要素は無視される
+    console.log(RubyParser.parse('｜漢字《かん,じ｜カン,ジ｜KAN,JI》')) // 二つ目以降の｜要素は無視される。一字ルビは最初だけ。
+    // 想定外↑↑↑↑↑
+
+    console.log(RubyParser.parse('｜漢字《かんじ｜カンジ》')) // 上下ルビ
+    console.log(RubyParser.parse('↓漢字《かんじ｜カンジ》')) // 上下ルビ
+    console.log(RubyParser.parse('｜漢字《かん,じ｜カンジ》')) // 上下ルビ（一字ルビ＋全体）
+    console.log(RubyParser.parse('↓漢字《かん,じ｜カンジ》')) // 上下ルビ（一字ルビ＋全体）
+    console.log(RubyParser.parse('｜漢字《かん,じ｜カン,ジ》')) // 一字ルビは最初だけ。最後は,が含まれてしまう
+    console.log(RubyParser.parse('｜漢字《かん,じ｜KAN,JI》')) // 一字ルビを上下共に必要とする場合もありうる？(ローマ字(発音記号))
+    console.log(RubyParser.parse('↓漢字《かん,じ｜KAN,JI》')) // 一字ルビを上下共に必要とする場合もありうる？(ローマ字(発音記号))
+
+    // kanjisTwin 上下少なくとも一方のルビが少ない
+    console.log(RubyParser.parse('｜漢字少《かん,じ,しょう｜KAN,JI》')) // 一字ルビを上下共に必要とする場合もありうる？(ローマ字(発音記号))
+    console.log(RubyParser.parse('｜漢字少《かん,じ｜KAN,JI,SHOU》')) // 一字ルビを上下共に必要とする場合もありうる？(ローマ字(発音記号))
+    console.log(RubyParser.parse('｜漢字少《かん,じ｜KAN,JI》')) // 一字ルビを上下共に必要とする場合もありうる？(ローマ字(発音記号))
+    console.log(RubyParser.parse('↓漢字少《かん,じ,しょう｜KAN,JI》')) // 一字ルビを上下共に必要とする場合もありうる？(ローマ字(発音記号))
+    console.log(RubyParser.parse('↓漢字少《かん,じ｜KAN,JI,SHOU》')) // 一字ルビを上下共に必要とする場合もありうる？(ローマ字(発音記号))
+    console.log(RubyParser.parse('↓漢字少《かん,じ｜KAN,JI》')) // 一字ルビを上下共に必要とする場合もありうる？(ローマ字(発音記号))
+
+
+    console.log(RubyParser.parse('山《やま》田《だ》'))
+    console.log(RubyParser.parse('太《た》郎《ろう》'))
+    console.log(RubyParser.parse('山田《やまだ》太郎《たろう》'))
+    console.log(RubyParser.parse('山田太郎《やまだたろう》'))
+    console.log(RubyParser.parse('山田太郎《やまだたろう｜Yamada Tarou》'))
+
+    // KanjiGroupRubyParser
+    console.log(RubyParser.parse('山田｜太郎《やまだ｜たろう》')) // 太郎に両ルビが振られ、山田はそのまま出力される
+    console.log(RubyParser.parse('あ山田｜太郎《やまだ｜たろう》')) // 太郎に両ルビが振られ、山田はそのまま出力される
+    console.log(RubyParser.parse('｜山田｜太郎《やまだ｜たろう》'))
+    console.log(RubyParser.parse('↓山田｜太郎《やまだ｜たろう》'))
+    console.log(RubyParser.parse('｜山田｜太郎《やまだ｜たろう｜ヤマさん》'))
+    console.log(RubyParser.parse('↓山田｜太郎《やまだ｜たろう｜ヤマさん》'))
+    console.log(RubyParser.parse('｜山田｜太郎《やま,だ｜た,ろう》'))
+    console.log(RubyParser.parse('↓山田｜太郎《やま,だ｜た,ろう》'))
+    console.log(RubyParser.parse('｜山田｜太郎《やまだたろう》')) // ルビ文字の｜が足りない！
+    console.log(RubyParser.parse('｜山田｜太郎《やま,｜,ろう》'))
+    console.log(RubyParser.parse('｜山田｜太郎《やま｜ろう》'))
+    //console.log(RubyParser.parse('｜山田下｜太郎助《やま,した｜ろう,すけ》')) // ルビ,不足
+    console.log(RubyParser.parse('｜山田下｜太郎助《やま,だ｜た,ろう》')) // ルビ,不足
+    console.log(RubyParser.parse('｜山田下｜太郎助《やま,だ｜た》')) // ルビ,不足
+    console.log(RubyParser.parse('｜山田｜太郎《やま,だ,した｜た,ろう,すけ》')) // ルビ,超過
+    console.log(RubyParser.parse('｜山田下｜太郎助《やま,だ,した｜た,ろう,すけ》'))
+    console.log(RubyParser.parse('｜おや｜もじ《OYA｜MOJI》')) // 親文字が漢字じゃない！
+    console.log(RubyParser.parse('｜おや｜文字《OYA｜MOJI》')) // 親文字が漢字じゃない！
+    console.log(RubyParser.parse('｜親｜もじ《OYA｜MOJI》')) // 親文字が漢字じゃない！
+    console.log(RubyParser.parse('｜改行《かいぎょう》\nが｜有《あ》る｜親｜もじ《OYA｜MOJI》')) // 親文字が漢字じゃない！
+//    console.log(RubyParser.parse('山田太郎《やま｜だ｜た｜ろう｜やまだたろう》')) // 不可能！！！
+//    console.log(RubyParser.parse('山田太郎《やま,だ,た,ろう》'))
+//    console.log(RubyParser.parse('山田太郎《やま,だ,た,ろう｜やまだたろう》'))
+//    console.log(RubyParser.parse('HTML《Hyper,Text,Markup,Language｜エイチティーエムエル》'))
+//    console.log(RubyParser.parse('一二〇〇《ひと,ふた,まる,まる｜じゅうにじ》'))
+
+    // KanjiRubyParser
+    console.log(RubyParser.parse('｜山田太郎《やま,だ,た,ろう》'))
+    console.log(RubyParser.parse('↓山田太郎《やま,だ,た,ろう》'))
+    console.log(RubyParser.parse('｜山田太郎《やま,だ》')) // 不足分ルビはそのまま親文字だけを出力してHTML化する
+    console.log(RubyParser.parse('｜山田太郎《やま,だ,た,ろう｜ヤマさん》'))
+    console.log(RubyParser.parse('↓山田太郎《やま,だ,た,ろう｜ヤマさん》'))
+    console.log(RubyParser.parse('↓山田太郎《やま,だ,た,ろう｜ヤマさん｜無視される》')) // 超過分ルビは無視される
+
+    // エスケープ
+    console.log(RubyParser.parse('|山田太郎《やま,だ,た,ろう》')) // Short
+//    console.log(RubyParser.parse('￬山田太郎《やま,だ,た,ろう》'))
+    console.log(RubyParser.parse('||あ山田太郎《やま,だ,た,ろう》')) // Long
+    console.log(RubyParser.parse('￬￬あ山田太郎《やま,だ,た,ろう》'))
+
+    // em
+    console.log(RubyParser.parse('ここを《《強調》》する。当然《とうぜん》｜漢字《かんじ》と併用も可能。'))
+    console.log(RubyParser.parse('ここを《《強調》》する。«強調»のエスケープも可能。当然《とうぜん》｜漢字《かんじ》と併用も可能。'))
+});
+window.addEventListener('beforeunload', (event) => {
+    console.log('beforeunload!!');
+});
+
---
+++ docs/30/test/pattern/long-warn.js
@@ -0,0 +1,41 @@
+function getRubyLongWarns() { return `
+｜漢字《》	｜漢字《》
+｜漢字《｜》	<ruby style="ruby-position:under;"><ruby style="ruby-position:over;">漢字<rp>（</rp><rt></rt><rp>）</rp></ruby><rp>（</rp><rt></rt><rp>）</rp></ruby>
+｜漢字《｜｜》	<ruby style="ruby-position:under;"><ruby style="ruby-position:over;">漢字<rp>（</rp><rt></rt><rp>）</rp></ruby><rp>（</rp><rt></rt><rp>）</rp></ruby>
+｜漢字《かんじ｜》	<ruby style="ruby-position:under;"><ruby style="ruby-position:over;">漢字<rp>（</rp><rt>かんじ</rt><rp>）</rp></ruby><rp>（</rp><rt></rt><rp>）</rp></ruby>
+｜漢字《｜カンジ》	<ruby style="ruby-position:under;"><ruby style="ruby-position:over;">漢字<rp>（</rp><rt></rt><rp>）</rp></ruby><rp>（</rp><rt>カンジ</rt><rp>）</rp></ruby>
+｜漢字《かんじ｜｜》	<ruby style="ruby-position:under;"><ruby style="ruby-position:over;">漢字<rp>（</rp><rt>かんじ</rt><rp>）</rp></ruby><rp>（</rp><rt></rt><rp>）</rp></ruby>
+｜漢字《｜カンジ｜》	<ruby style="ruby-position:under;"><ruby style="ruby-position:over;">漢字<rp>（</rp><rt></rt><rp>）</rp></ruby><rp>（</rp><rt>カンジ</rt><rp>）</rp></ruby>
+｜漢字《｜｜カンジ》	<ruby style="ruby-position:under;"><ruby style="ruby-position:over;">漢字<rp>（</rp><rt></rt><rp>）</rp></ruby><rp>（</rp><rt></rt><rp>）</rp></ruby>
+｜漢字《,》	<ruby>漢<rp>（</rp><rt></rt><rp>）</rp></ruby><ruby>字<rp>（</rp><rt></rt><rp>）</rp></ruby>
+｜漢字《かん,》	<ruby>漢<rp>（</rp><rt>かん</rt><rp>）</rp></ruby><ruby>字<rp>（</rp><rt></rt><rp>）</rp></ruby>
+｜漢字《,じ》	<ruby>漢<rp>（</rp><rt></rt><rp>）</rp></ruby><ruby>字<rp>（</rp><rt>じ</rt><rp>）</rp></ruby>
+｜漢字《かん,じ,超過》	<ruby>漢<rp>（</rp><rt>かん</rt><rp>）</rp></ruby><ruby>字<rp>（</rp><rt>じ</rt><rp>）</rp></ruby>
+｜漢字《かんじ｜カンジ｜超過》	<ruby style="ruby-position:under;"><ruby style="ruby-position:over;">漢字<rp>（</rp><rt>かんじ</rt><rp>）</rp></ruby><rp>（</rp><rt>カンジ</rt><rp>）</rp></ruby>
+｜漢字《かん,じ｜KAN,JI｜超,過》	<ruby style="ruby-position:under;"><ruby style="ruby-position:over;">漢<rp>（</rp><rt>かん</rt><rp>）</rp></ruby><rp>（</rp><rt>KAN</rt><rp>）</rp></ruby><ruby style="ruby-position:under;"><ruby style="ruby-position:over;">字<rp>（</rp><rt>じ</rt><rp>）</rp></ruby><rp>（</rp><rt>JI</rt><rp>）</rp></ruby>
+
+｜山田太郎《やま,だ》	<ruby>山<rp>（</rp><rt>やま</rt><rp>）</rp></ruby><ruby>田<rp>（</rp><rt>だ</rt><rp>）</rp></ruby>太郎
+｜山田太郎《やま,だ,た,ろう,超過》	<ruby>山<rp>（</rp><rt>やま</rt><rp>）</rp></ruby><ruby>田<rp>（</rp><rt>だ</rt><rp>）</rp></ruby><ruby>太<rp>（</rp><rt>た</rt><rp>）</rp></ruby><ruby>郎<rp>（</rp><rt>ろう</rt><rp>）</rp></ruby>
+｜山田｜太郎《やまだたろう》	<ruby>山田<rp>（</rp><rt>やまだたろう</rt><rp>）</rp></ruby>太郎
+｜山田｜太郎《やま,｜,ろう》	<ruby>山<rp>（</rp><rt>やま</rt><rp>）</rp></ruby><ruby>田<rp>（</rp><rt></rt><rp>）</rp></ruby><ruby>太<rp>（</rp><rt></rt><rp>）</rp></ruby><ruby>郎<rp>（</rp><rt>ろう</rt><rp>）</rp></ruby>
+｜山田｜太郎《やま｜ろう》	<ruby>山田<rp>（</rp><rt>やま</rt><rp>）</rp></ruby><ruby>太郎<rp>（</rp><rt>ろう</rt><rp>）</rp></ruby>
+｜山田下｜太郎助《やま,だ｜た,ろう》	<ruby>山<rp>（</rp><rt>やま</rt><rp>）</rp></ruby><ruby>田<rp>（</rp><rt>だ</rt><rp>）</rp></ruby>下<ruby>太<rp>（</rp><rt>た</rt><rp>）</rp></ruby><ruby>郎<rp>（</rp><rt>ろう</rt><rp>）</rp></ruby>助
+｜山田下｜太郎助《やま,だ｜た》	<ruby>山<rp>（</rp><rt>やま</rt><rp>）</rp></ruby><ruby>田<rp>（</rp><rt>だ</rt><rp>）</rp></ruby>下<ruby>太<rp>（</rp><rt>た</rt><rp>）</rp></ruby>郎助
+｜山田｜太郎《やま,だ,した｜た,ろう,すけ》	<ruby>山<rp>（</rp><rt>やま</rt><rp>）</rp></ruby><ruby>田<rp>（</rp><rt>だ</rt><rp>）</rp></ruby><ruby>太<rp>（</rp><rt>た</rt><rp>）</rp></ruby><ruby>郎<rp>（</rp><rt>ろう</rt><rp>）</rp></ruby>
+
+｜山田｜太郎《やまだ｜たろう｜ヤマさん｜超過》	<ruby style="ruby-position:under;"><ruby style="ruby-position:over;">山田<rp>（</rp><rt>やまだ</rt><rp>）</rp></ruby><ruby style="ruby-position:over;">太郎<rp>（</rp><rt>たろう</rt><rp>）</rp></ruby><rp>（</rp><rt>ヤマさん</rt><rp>）</rp></ruby>
+
+｜山田｜太郎《やま,だ｜た,ろう｜ヤ,マ,さ,ん》	<ruby style="ruby-position:under;"><ruby style="ruby-position:over;">山<rp>（</rp><rt>やま</rt><rp>）</rp></ruby><rp>（</rp><rt>ヤ</rt><rp>）</rp></ruby><ruby style="ruby-position:under;"><ruby style="ruby-position:over;">田<rp>（</rp><rt>だ</rt><rp>）</rp></ruby><rp>（</rp><rt>マ</rt><rp>）</rp></ruby><ruby style="ruby-position:under;"><ruby style="ruby-position:over;">太<rp>（</rp><rt>た</rt><rp>）</rp></ruby><rp>（</rp><rt>さ</rt><rp>）</rp></ruby><ruby style="ruby-position:under;"><ruby style="ruby-position:over;">郎<rp>（</rp><rt>ろう</rt><rp>）</rp></ruby><rp>（</rp><rt>ん</rt><rp>）</rp></ruby>
+｜山田｜太郎《やま,だ｜た,ろう｜ヤ,マ,さ,ん,超過》	<ruby style="ruby-position:under;"><ruby style="ruby-position:over;">山<rp>（</rp><rt>やま</rt><rp>）</rp></ruby><rp>（</rp><rt>ヤ</rt><rp>）</rp></ruby><ruby style="ruby-position:under;"><ruby style="ruby-position:over;">田<rp>（</rp><rt>だ</rt><rp>）</rp></ruby><rp>（</rp><rt>マ</rt><rp>）</rp></ruby><ruby style="ruby-position:under;"><ruby style="ruby-position:over;">太<rp>（</rp><rt>た</rt><rp>）</rp></ruby><rp>（</rp><rt>さ</rt><rp>）</rp></ruby><ruby style="ruby-position:under;"><ruby style="ruby-position:over;">郎<rp>（</rp><rt>ろう</rt><rp>）</rp></ruby><rp>（</rp><rt>ん</rt><rp>）</rp></ruby>
+｜山田｜太郎《やま,だ｜た,ろう｜ヤ,マ,さ》	<ruby style="ruby-position:under;"><ruby style="ruby-position:over;">山<rp>（</rp><rt>やま</rt><rp>）</rp></ruby><ruby style="ruby-position:over;">田<rp>（</rp><rt>だ</rt><rp>）</rp></ruby><ruby style="ruby-position:over;">太<rp>（</rp><rt>た</rt><rp>）</rp></ruby><ruby style="ruby-position:over;">郎<rp>（</rp><rt>ろう</rt><rp>）</rp></ruby><rp>（</rp><rt>ヤマさ</rt><rp>）</rp></ruby>
+｜山田｜太郎《やま,だ｜た,ろう｜ヤマ,ダ｜タ,ロウ》	<ruby style="ruby-position:under;"><ruby style="ruby-position:over;">山<rp>（</rp><rt>やま</rt><rp>）</rp></ruby><rp>（</rp><rt>ヤマ</rt><rp>）</rp></ruby><ruby style="ruby-position:under;"><ruby style="ruby-position:over;">田<rp>（</rp><rt>だ</rt><rp>）</rp></ruby><rp>（</rp><rt>ダ</rt><rp>）</rp></ruby><ruby style="ruby-position:under;"><ruby style="ruby-position:over;">太<rp>（</rp><rt>た</rt><rp>）</rp></ruby><rp>（</rp><rt>タ</rt><rp>）</rp></ruby><ruby style="ruby-position:under;"><ruby style="ruby-position:over;">郎<rp>（</rp><rt>ろう</rt><rp>）</rp></ruby><rp>（</rp><rt>ロウ</rt><rp>）</rp></ruby>
+
+｜漢字少《かん,じ,しょう｜KAN,JI》	<ruby style="ruby-position:under;"><ruby style="ruby-position:over;">漢<rp>（</rp><rt>かん</rt><rp>）</rp></ruby><rp>（</rp><rt>KAN</rt><rp>）</rp></ruby><ruby style="ruby-position:under;"><ruby style="ruby-position:over;">字<rp>（</rp><rt>じ</rt><rp>）</rp></ruby><rp>（</rp><rt>JI</rt><rp>）</rp></ruby><ruby>少<rp>（</rp><rt>しょう</rt><rp>）</rp></ruby>
+｜漢字少《かん,じ｜KAN,JI,SHOU》	<ruby style="ruby-position:under;"><ruby style="ruby-position:over;">漢<rp>（</rp><rt>かん</rt><rp>）</rp></ruby><rp>（</rp><rt>KAN</rt><rp>）</rp></ruby><ruby style="ruby-position:under;"><ruby style="ruby-position:over;">字<rp>（</rp><rt>じ</rt><rp>）</rp></ruby><rp>（</rp><rt>JI</rt><rp>）</rp></ruby><ruby style="ruby-position:under;">少<rp>（</rp><rt>SHOU</rt><rp>）</rp></ruby>
+｜漢字少《かん,じ｜KAN,JI》	<ruby style="ruby-position:under;"><ruby style="ruby-position:over;">漢<rp>（</rp><rt>かん</rt><rp>）</rp></ruby><rp>（</rp><rt>KAN</rt><rp>）</rp></ruby><ruby style="ruby-position:under;"><ruby style="ruby-position:over;">字<rp>（</rp><rt>じ</rt><rp>）</rp></ruby><rp>（</rp><rt>JI</rt><rp>）</rp></ruby>少
+↓漢字少《かん,じ,しょう｜KAN,JI》	<ruby style="ruby-position:over;"><ruby style="ruby-position:under;">漢<rp>（</rp><rt>かん</rt><rp>）</rp></ruby><rp>（</rp><rt>KAN</rt><rp>）</rp></ruby><ruby style="ruby-position:over;"><ruby style="ruby-position:under;">字<rp>（</rp><rt>じ</rt><rp>）</rp></ruby><rp>（</rp><rt>JI</rt><rp>）</rp></ruby><ruby style="ruby-position:under;">少<rp>（</rp><rt>しょう</rt><rp>）</rp></ruby>
+↓漢字少《かん,じ｜KAN,JI,SHOU》	<ruby style="ruby-position:over;"><ruby style="ruby-position:under;">漢<rp>（</rp><rt>かん</rt><rp>）</rp></ruby><rp>（</rp><rt>KAN</rt><rp>）</rp></ruby><ruby style="ruby-position:over;"><ruby style="ruby-position:under;">字<rp>（</rp><rt>じ</rt><rp>）</rp></ruby><rp>（</rp><rt>JI</rt><rp>）</rp></ruby><ruby style="ruby-position:over;">少<rp>（</rp><rt>SHOU</rt><rp>）</rp></ruby>
+↓漢字少《かん,じ｜KAN,JI》	<ruby style="ruby-position:over;"><ruby style="ruby-position:under;">漢<rp>（</rp><rt>かん</rt><rp>）</rp></ruby><rp>（</rp><rt>KAN</rt><rp>）</rp></ruby><ruby style="ruby-position:over;"><ruby style="ruby-position:under;">字<rp>（</rp><rt>じ</rt><rp>）</rp></ruby><rp>（</rp><rt>JI</rt><rp>）</rp></ruby>少
+
+` }
+
